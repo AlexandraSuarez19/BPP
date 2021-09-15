f67cd4715124ce8a23635102e987a6a2092921e3
Alexandra Suarez <alexsuarez19@hotmail.com>
2021-09-16 01:17:43 +0200

    Primera subida listas

---
+++ trabajolistas.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 15:25:20 2021
+
+@author: toshiba
+"""
+
+
+
+letras = []
+
+for i in "Python es el mejor lenguaje de programación":
+    if(i != 'm'):
+        letras.append(i)
+    
+print(letras)
+
+
