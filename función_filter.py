fe759c754eb288c144bc50a305a8f7e00fad722c
Alexandra Suarez <alexsuarez19@hotmail.com>
2021-09-16 01:37:40 +0200

    ejemplo con filter

--- función_filter.py
+++ función_filter.py
@@ -1,9 +1,8 @@
 elementos = [1,2,3,4,5,6,7,8]
 
-pares = []
+def es_par(n):
+    return n%2==0
 
-for i in elementos:
-    if (i%2 == 0):
-        pares.append(i)
+pares = list(filter(es_par, elementos))
 
 print(pares)
