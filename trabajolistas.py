6a0617b8a21e21379df84235b60f0a2306155e33
Alexandra Suarez <alexsuarez19@hotmail.com>
2021-09-16 01:23:19 +0200

    uso de compresión de listas

--- trabajolistas.py
+++ trabajolistas.py
@@ -7,11 +7,7 @@ Created on Wed Sep 15 15:25:20 2021
 
 
 
-letras = []
-
-for i in "Python es el mejor lenguaje de programación":
-    if(i != 'm'):
-        letras.append(i)
+letras = [i for i in "Python es el mejor lenguaje de programación" if i != "m"]
     
 print(letras)
 
