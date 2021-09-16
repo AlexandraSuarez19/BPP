468d47567b330d39ea6500051835fcdcca395770
Alexandra Suarez <alexsuarez19@hotmail.com>
2021-09-16 02:14:04 +0200

    main creado para documentar

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 13 19:16:16 2021
+
+@author: toshiba
+"""
+
+from utils import ejemplo_operaciones
+
+if __name__ == '__main__':
+    m = ejemplo_operaciones.operacionesMatematicas(5,6)
+    print(m.suma())
+    
